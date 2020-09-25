7759dd6111f0666148155c4aa86d722f143c0e56
Edwin Guerrero <edwinguerrerotech@gmail.com>
2020-09-25 16:29:33 -0400

    If the device doesn't have a camera

--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraBtn: UIBarButtonItem!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // disable the camera if the device being used doesn't have one.
+        cameraBtn.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func pickAnImage(_ sender: Any) {
+    @IBAction func pickAnImageFromPhotoLibrary(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
     }
     
